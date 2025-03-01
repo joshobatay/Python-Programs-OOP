54ec0aa7a95245adef5ee25761c59850ec56c8db
Josh Obatay <joshobatay2005@gmail.com>
2025-03-01 17:22:10 +0800

    codes

--- Prog01/Prog01.py
+++ Prog01/Prog01.py
@@ -1,19 +1,15 @@
 # Create a program that ask user to input 2 numbers. Print the bigger number.
 
-
-def print_number():
+try:
     
-    try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    if num1 > num2:
+        print("The bigger number is", num1)
+    else:
+        print("The bigger number is", num2)  
         
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
+except ValueError:
+    print("Please enter a valid number")
 
-        if num1 > num2:
-            print("The bigger number is", num1)
-        else:
-            print("The bigger number is", num2)
-            
-    except ValueError:
-        print("Please enter a valid number")
-
-print_number()
