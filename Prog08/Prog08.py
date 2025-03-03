08ba6aca1248b4b819b0f257b701fe4ab58b4132
Josh Obatay <joshobatay2005@gmail.com>
2025-03-03 14:59:55 +0800

    codes

--- Prog08/Prog08.py
+++ Prog08/Prog08.py
@@ -1,22 +1,17 @@
 # Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
-'''
+odd_count = 0
 
-odd_count = 0  
-
-for i in range(10):  
+for i in range(10):
     while True:
         try:
-            num = int(input(f"Enter number {i+1}: "))  
-            if num % 2 != 0:  
-                odd_count += 1  
-            break  
+            num = int(input(f'Enter number {i+1}: '))
+            if num % 2 != 0:
+                odd_count += 1
+            break
         except ValueError:
-            print("Invalid input! Please enter an integer.")
+            print('Please enter a valid number')
 
-print(f"The number of odd numbers entered is {odd_count}")
+print(f'There are {odd_count} odd numbers')
 
-
-
-'''
 
